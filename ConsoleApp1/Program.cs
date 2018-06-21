32d5179b14ce149e9d2289e6b34463ef230aab18
InfamousGamer <emekasolo901@gmail.com>
2018-06-20 23:35:06 -0700

    initial commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,11 +2,41 @@
 
 namespace ConsoleApp1
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Console.WriteLine("Welcome To LongRun Studios!");
+      System.Threading.Thread.Sleep(2000);
+      Console.WriteLine("The Servers are booting up, please be patient.");
+      System.Threading.Thread.Sleep(1000);
+      Console.WriteLine("(.)");
+      System.Threading.Thread.Sleep(1000);
+    Console.WriteLine("(..)");
+      System.Threading.Thread.Sleep(1000);
+      Console.WriteLine("(...)");
+      System.Threading.Thread.Sleep(1000);
+      Console.WriteLine("(.)");
+      System.Threading.Thread.Sleep(1000);
+      Console.WriteLine("(..)");
+      System.Threading.Thread.Sleep(1000);
+      Console.WriteLine("(...)");
+      System.Threading.Thread.Sleep(1000);
+      Console.WriteLine("(.)");
+      System.Threading.Thread.Sleep(1000);
+      Console.WriteLine("(..)");
+      System.Threading.Thread.Sleep(1000);
+      Console.WriteLine("(...)");
+      System.Threading.Thread.Sleep(1000);
+      Console.WriteLine("(.)");
+      System.Threading.Thread.Sleep(1000);
+      Console.WriteLine("(..)");
+      System.Threading.Thread.Sleep(5000);
+      Console.WriteLine("To Be Continued...");
+      var c = Console.Read();
+      while (c != (int)'b')
+        c = Console.Read();
+
+      }
     }
-}
+  }
